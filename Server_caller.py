c80e9899820aff0eb91c30cde775b80e5e6c76a8
ChatMetamaterials <zhenyanggao@sjtu.edu.cn>
2025-03-25 12:57:17 +0800

    Update Server_caller.py

--- Server_caller.py
+++ Server_caller.py
@@ -1,7 +1,1 @@
 from source_server_interface import *
-
-file_name = "irrational_architectural_data.txt"
-metadata_extended = extract_data_txt("Example_universal_metamaterial.txt")
-Type = "irrational"
-architectures = extended_architecture_data_algorithms(metadata_extended, Type)
-write_json_to_txt(architectures, file_name)
